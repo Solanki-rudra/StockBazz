05556bd7aa81c7c5c6f5c22617395b3cd5c13d87
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2024-07-22 19:10:36 +0530

    remove extra files from stockbazz

--- Stockbaszz/Stockbaszz/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Stockbaszz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
